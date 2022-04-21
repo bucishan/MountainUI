b2e3b0ba8b4d6fe1485ff08a7793f663294acbd0
iMountains <i@shanli.ren>
2022-04-21 13:13:57 +0800

    test push

---
+++ MountainUI/Common/MGDI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mountain.UI
+{
+    public static class MGDI
+    {
+    }
+}
